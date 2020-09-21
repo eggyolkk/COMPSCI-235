ad45c7e1bae3ac0a702a9347bb8e219452fcabf5
eggyolkk <ellabanaticla@gmail.com>
2020-09-21 22:12:19 +1200

    Updating blueprints

--- people_web_app/people_blueprint/people.py
+++ people_web_app/people_blueprint/people.py
@@ -4,11 +4,15 @@ from wtforms import IntegerField, SubmitField
 from wtforms.validators import DataRequired
 
 import people_web_app.adapters.repository as repo
+from people_web_app import Person
 
 people_blueprint = Blueprint(
     'people_bp', __name__
 )
 
+class SearchForm(FlaskForm):
+    person_id = IntegerField('Person id')
+    submit = SubmitField('Find')
 
 @people_blueprint.route('/')
 def home():
@@ -21,12 +25,22 @@ def home():
 
 @people_blueprint.route('/list')
 def list_people():
-    pass
+    html_page = render_template('list_people.html', people=repo.repo_instance, person=Person)
+    return html_page
 
 
 @people_blueprint.route('/find', methods=['GET', 'POST'])
 def find_person():
-    pass
+    form = SearchForm()
+
+    if form.validate_on_submit():
+        try:
+            data = request.form.get('person_id.data')
+            for person in repo.repo_instance:
+                if data in person:
+                    match = person
+            html_page = render_template('list_person.html', person=Person)
+            return html_page
 
 
 
